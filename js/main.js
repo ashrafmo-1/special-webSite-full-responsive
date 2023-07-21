2cf57c42bd7b2b83b2dbc7457d2b5479e5c172ef
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-07-21 06:43:21 +0300

    add Edit Site Options can Edit color And image

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// section setting
 let settingBox = document.querySelector(".settingBox")
 settingBox.onclick = function() {
     this.classList.toggle("open")
@@ -6,25 +7,21 @@ settingBox.onclick = function() {
 // change color
 let colorsChanger = document.querySelectorAll(".changeColor span");
 colorsChanger.forEach(span => {
-        span.addEventListener("click", function(ele) {
-            console.log(ele.target.dataset.color) // for testing only
-            document.documentElement.style.setProperty("--text-main-color", ele.target.dataset.color)
-                // set color to local storage
-            localStorage.setItem("color_option", ele.target.dataset.color)
-
+    span.addEventListener("click", function(ele) {
+        console.log(ele.target.dataset.color) // for testing only
+        document.documentElement.style.setProperty("--text-main-color", ele.target.dataset.color)
+            // set color to local storage
+        localStorage.setItem("color_option", ele.target.dataset.color)
             // remove Active class from al Element
-            ele.target.parentElement.querySelectorAll(".active").forEach(el => {
-                    el.classList.remove("active")
-                })
-                // add Active class From Target Element
-            ele.target.classList.add("active")
-                //     // set Active class to local Storage
-                // localStorage.setItem("ActiveColor", ele.target.classList)
-        })
+        ele.target.parentElement.querySelectorAll(".active").forEach(el => {
+                el.classList.remove("active")
+            })
+            // add Active class From Target Element
+        ele.target.classList.add("active")
     })
-    // get color from localStorage
+})
 
-
+// get color from localStorage
 let myColors = localStorage.getItem("color_option");
 if (myColors !== null) {
     console.log("have color on local"); // To make sure that color
@@ -39,15 +36,67 @@ if (myColors !== null) {
         }
     })
 }
-// add ANd remove class Active from changer Background
-let backGroundChanger = document.querySelectorAll(".select div");
-backGroundChanger.forEach(div => {
-    div.addEventListener("click", function(ele) {
+// add ANd remove class Active from changer random Background
+let backGroundChanger = document.querySelectorAll(".select span");
+backGroundChanger.forEach(span => {
+    span.addEventListener("click", function(ele) {
         // remove Active class from al Element
-        ele.target.parentElement.querySelectorAll(".active").forEach(el => {
-                el.classList.remove("active")
+        ele.target.parentElement.querySelectorAll(".active").forEach(ele => {
+                ele.classList.remove("active")
             })
             // add Active class From Target Element
-        ele.target.classList.add("active")
+        ele.target.classList.add("active");
+        // Stop or stay random
+        if (ele.target.dataset.background === 'yes') {
+            backgroundOption = true;
+            randomImages()
+            localStorage.setItem("chacger-backGround", true)
+        } else {
+            backgroundOption = false;
+            clearInterval(backgroundInterval)
+            localStorage.setItem("chacger-backGround", false)
+        };
     });
 });
+let backgroundOption = true; // true => Boolian Value
+// constrol the interval (must catch to control it)
+let backgroundInterval;
+// remove Active class from al Element from localStorage
+let backgroundlocalStorage = localStorage.getItem("chacger-backGround");
+// chick random in localStorage is not Empty
+if (backgroundlocalStorage !== null) {
+    if (backgroundlocalStorage === 'true') {
+        backgroundOption = true
+    } else {
+        backgroundOption = false
+    }
+
+    // remove Active class from all spans
+    let allSpans = document.querySelectorAll(".select span")
+    allSpans.forEach((ele) => {
+        ele.classList.remove("active")
+    })
+    if (backgroundlocalStorage === 'true') {
+        let yes = document.querySelector(".select .yes")
+        yes.classList.add("active")
+    } else {
+        let yes = document.querySelector(".select .no")
+        yes.classList.add("active")
+    }
+
+}
+// change Bg After a custom time
+let landingBg = document.querySelector(".landing");
+let allImage = ["../image/1.jpg", "../image/2.jpg", "../image/4.jpg", "../image/5.jpg", "../image/6.jpg"];
+// random background option
+function randomImages() {
+    if (backgroundOption === true) {
+        backgroundInterval = setInterval(() => {
+            // random length number
+            let randomnum = Math.floor(Math.random() * allImage.length);
+            // console.log(randomnum) // test
+            landingBg.style.backgroundImage = 'url("imgs/' + allImage[randomnum] + '")';
+        }, 1000);
+    };
+};
+randomImages()
