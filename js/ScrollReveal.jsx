f6be8e3c6d95186b42591d5bea01e7ddca998215
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-07-24 02:05:24 +0300

    make libirary ScrollReveal from animation

---
+++ js/ScrollReveal.jsx
@@ -0,0 +1,20 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+sr.reveal(`.brandName`, { origin: 'left', interval: 100, delay: 100, distance: '10px' })
+sr.reveal(`nav`, { origin: 'right', interval: 100, delay: 100, distance: '10px' })
+sr.reveal(`.landing h1`)
+sr.reveal(`.landing .content p`, { origin: 'top', interval: 100, delay: 100, distance: '10px' })
+sr.reveal(`.AboutUs h1`)
+sr.reveal(`.AboutUs .text-content p`, { origin: 'top', interval: 100, delay: 100, distance: '10px' })
+sr.reveal(`.AboutUs .image img`, { origin: 'right', interval: 100, delay: 200, distance: '60px' })
+sr.reveal(`.allBoxesSkills .box, .main-heading`)
+sr.reveal(`.galary .box`, { origin: 'bottom', interval: 100, delay: 200, distance: '60px' })
+sr.reveal(`.timeline .left`,{ origin: 'left', interval: 100, delay: 100, distance: '10px' })
+sr.reveal(`.timeline .right`,{ origin: 'right', interval: 200, delay: 300, distance: '30px' })
+sr.reveal(`.features .box`, { origin: 'right', interval: 100, delay: 200, distance: '60px' })
+sr.reveal(`.container .box`,)
